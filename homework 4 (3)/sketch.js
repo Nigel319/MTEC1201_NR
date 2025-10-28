d8fc36e6144f5afaa161e2f89e9a30cdc193714d
Nigel319 <nigel.richards@mail.citytech.cuny.edu>
2025-10-28 01:48:27 -0400

    Midterm_WIP_NR

--- homework 4 (3)/sketch.js
+++ homework 4 (3)/sketch.js
@@ -2,20 +2,32 @@ let redVal = 0;
 let greenVal = 0 
 
 let sunHeight = 600;
+let sunX
 
 function setup() {
-  createCanvas(600,400);
+  createCanvas(600,400, WEBGL);
   noStroke();
 
 }
 
+
+
 function draw() {
   background(redVal, greenVal, 0);
 
+  let options = {
+    disableTouchActions: false,
+    freeRotation: true
+  };
+
+  orbitControl (1, 1 ,1, options);
+
+  normalMaterial();
+
   fill(255, 135, 5, 60);
-  circle(300, sunHeight, 180);
+  circle(sunX, sunHeight, 180);
   fill(255, 100, 0, 100);
-  circle(300, sunHeight, 140);
+  circle(sunX, sunHeight, 140);
 
   fill(110, 50, 18);
   triangle(200, 400, 520, 253, 800, 400);
@@ -32,14 +44,20 @@ function draw() {
   fill(120, 80, 50);
   triangle(200, 400, 450, 250, 300, 400)
 
-  if(sunHeight > 140){
-    sunHeight -= 2;
+//   if(sunHeight > 140){
+//     sunHeight -= 2;
   
-  if(sunHeight > 480) {
-    redVal += 4;
-    greenVal += 1;
-  }
-}
-console.log(sunHeight);
-  
+//   if(sunHeight > 480) {
+//     redVal += 4;
+//     greenVal += 1;
+//   }
+// }
+
+greenVal = map( sin(frameCount/100),-1,1,0,255)
+redVal = map( sin(frameCount/100),-1,1,0,255)
+
+sunHeight = map( sin(frameCount/100),-1,1, height,0)
+sunX = map( cos(frameCount/100),-1,1, width, width/2)
+
+
 }
